b4b833e43520f95a7248b2ed34b1b34f1087d566
CelestialBoon <tosxychor@gmail.com>
2021-12-05 11:53:53 +0100

    d05 2021

---
+++ 2021/src/d03.rs
@@ -0,0 +1,87 @@
+use std::fs::File;
+use std::io::{prelude::*, BufReader};
+use std::collections::VecDeque;
+// use regex::Regex;
+
+fn main() {
+    let reader = BufReader::new(File::open("inputs/i03.txt").unwrap());
+
+    let mut size = 0;
+    let mut char_list : Vec<Vec<char>> = Vec::new();
+    
+    //parsing
+    for line in reader.lines() {
+        let l = line.unwrap();
+        if size == 0 { size = l.len(); }
+        char_list.push(l.chars().collect::<Vec<_>>());
+    }
+
+    fn zeros_ones(list: &Vec<Vec<char>>, position: usize) -> (i32, i32) {
+        let (mut zeros, mut ones) = (0, 0);
+        for n in 0..list.len() {
+            match list[n][position] {
+                '0' => zeros += 1,
+                '1' => ones += 1,
+                _ => { panic!("not zero or one"); }
+            }
+        }
+        (zeros, ones)
+    }
+    
+    //decoding binary
+    fn to_number(vec : &Vec<char>) -> i32 {
+        let st = vec.iter().collect::<String>();
+        // println!("{}", st);
+        i32::from_str_radix(&st, 2).unwrap()
+    }
+    
+    //gammaepsilon
+    {
+        let mut vgamma : VecDeque<char> = VecDeque::new();
+        let mut vepsilon : VecDeque<char> = VecDeque::new();
+
+        //double iteration
+        for i in (0..size).rev() {
+            let (zeros, ones) = zeros_ones(&char_list, i);
+            if zeros > ones {
+                vgamma.push_front('0');
+                vepsilon.push_front('1');
+            } else {
+                vgamma.push_front('1');
+                vepsilon.push_front('0');
+            }
+        }
+
+        let gamma = to_number(&vgamma.into_iter().collect());
+        let epsilon = to_number(&vepsilon.into_iter().collect());
+        
+        // println!("{} - {}", );
+        println!("gamma*epsilon = {}", gamma*epsilon);
+    }
+    //O2 CO2
+    {
+        enum Rating { Oxygen, Co2 }
+
+        fn scrematura(olist : &Vec<Vec<char>>, length: usize, rating: Rating) -> Vec<char> {
+            let mut list = olist.clone();
+            for i in 0..length {
+                let (zeros, ones) = zeros_ones(&list, i);
+                let criteria = match &rating {
+                    Rating::Oxygen => {if ones >= zeros {'1'} else {'0'}}
+                    Rating::Co2 => {if ones >= zeros {'0'} else {'1'}}
+                };
+                list = list.into_iter().filter(|l| l[i] == criteria).collect();
+                if list.len() == 1 { break; }
+            }
+            list[0].clone()
+        }
+
+        let vo2 = scrematura(&char_list, size, Rating::Oxygen);
+        let vco2 = scrematura(&char_list, size, Rating::Co2);
+        let o2 = to_number(&vo2);
+        let co2 = to_number(&vco2);
+        
+        println!("o2*co2 = {}", o2*co2);
+    }    
+}
+
---
+++ 2021/src/d04.rs
@@ -0,0 +1,91 @@
+use std::fs::File;
+use std::io::{prelude::*, BufReader};
+use std::collections::HashMap;
+// use std::collections::VecDeque;
+// use regex::Regex;
+
+//each number has reference to what boards
+//and each board has 25 numbers with yes/no (dict?), 5 rows and 5 columns (10 arrays of numbers will do)
+//then each number you go to the refrence, update the dict, check those rows and columns
+//and when you get a hit on those you're done, you just check the dict for the unused numbers and bob's your uncle
+
+fn main() {
+    let reader = BufReader::new(File::open("inputs/i04.txt").unwrap());
+
+    let mut iter = reader.lines();
+    let mut b_firstline = true;
+    let mut firstline: Vec<isize> = Vec::new();
+
+    fn parse_numbers(s:String) -> Vec<isize> {
+        s.split_whitespace().map(|n| n.parse::<isize>().unwrap()).collect()
+    }
+
+    let mut numtobingo: HashMap<isize, Vec<isize>> = HashMap::new();
+    let mut bblist:Vec<BingoBoard> = Vec::new();
+    
+    //parsing
+    let mut bb: BingoBoard;
+    loop {
+        match iter.next() {
+            None => break,
+            Some(Ok(s)) => {
+                if b_firstline {
+                    firstline = s.split(',').map(|n| n.parse::<isize>().unwrap()).collect();
+                    b_firstline = false;
+                } else {
+                    let rows: Vec<Vec<isize>> = (0..5).map(|_| parse_numbers(iter.next().unwrap().unwrap())).collect();
+                    let numbers: Vec<isize> = rows.iter().flatten().copied().collect();
+                    let columns: Vec<Vec<isize>> = (0..5).map(|n1| (0..5).map(|n2| rows[n2][n1]).collect()).collect();
+                    let mut bingo_map : HashMap<isize, bool> = HashMap::new();
+                    for n in &numbers {
+                        bingo_map.insert(*n, false);           
+                    };
+                    
+                    bb = BingoBoard {
+                        rows: rows,
+                        columns: columns,
+                        bingo_map: bingo_map,
+                        won: false
+                    };
+                    //add all to some struct
+                    bblist.push(bb);
+                    let len = (bblist.len() as isize) - 1;
+                    
+                    //add the struct to the numtobingo
+                    for n in &numbers {
+                        numtobingo.entry(*n).or_insert(Vec::new()).push(len);
+                    };
+                }
+            },
+            _ => panic!("reading error")
+        }
+    }
+    let numboards = bblist.len();
+    let mut boardswon = 0;
+
+    'outer: for n in firstline {
+        for bbn in &numtobingo[&n] {
+            bblist.get_mut(*bbn as usize).unwrap().bingo_map.insert(n, true);
+        }
+
+        for bbn in &numtobingo[&n] {
+            let bb = &mut bblist[*bbn as usize];
+            if !bb.won && bb.rows.iter().chain(bb.columns.iter()).any(|rc| rc.iter().all(|n| bb.bingo_map[n])) {
+                boardswon += 1;
+                bb.won = true;
+                if boardswon == numboards || boardswon == 1 {
+                    let unmarkedsum = bb.bingo_map.iter().filter_map(|(n, marked)| (!marked).then(|| *n)).fold(0, |acc, n| acc+n);
+                    println!("{} * {} = {}", n, unmarkedsum, unmarkedsum*n);
+                    if boardswon == numboards {break 'outer;}
+                }
+            }
+        }
+    }
+}
+
+struct BingoBoard {
+    rows: Vec<Vec<isize>>,
+    columns: Vec<Vec<isize>>,
+    bingo_map: HashMap<isize, bool>,
+    won: bool
+}
