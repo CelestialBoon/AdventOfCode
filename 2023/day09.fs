54dc9d3561d85c877ee15b75a624bcc338fe7601
Mystes <dragosandalo@gmail.com>
2023-12-09 10:20:56 +0100

    AoC2023 day9

--- 2023/day09.fs
+++ 2023/day09.fs
@@ -1,7 +1,26 @@
 module Day09
 open Helpers
+let numberRx = System.Text.RegularExpressions.Regex @"-?\d+"
+
+let solve (lines:string[]) =
+    let nums = lines |> Array.map (fun s -> seq { for m in numberRx.Matches s -> m.Value |> int64} |> Seq.toList)
+
+    let rec sideNums (ns:int64 list) = 
+        let first, last = List.item 0 ns, List.last ns
+        let diffs = ns |> List.pairwise |> List.map (fun (n1, n2) -> n2 - n1)
+        if diffs |> List.forall ((=) 0L) then (first, last) else 
+            let before, after = sideNums diffs
+            (first - before, last + after)
+
+    let sides = nums |> Array.map sideNums
+    sides |> Array.map snd |> Array.sum, sides |> Array.map fst |> Array.sum
+    
+let example1 = [| "0 3 6 9 12 15" 
+; "1 3 6 10 15 21" 
+; "10 13 16 21 30 45" 
+|]
 
 let solution = {
-    Solve = (fun lines -> (0L,0L))
-    Check = myAssert (0L,0L) (0L,0L)
+    Solve = solve
+    Check = myAssert (solve example1) (114L,2L)
 }
